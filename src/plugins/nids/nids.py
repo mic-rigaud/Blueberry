9bbfa79be1f62f5b9ab4bcb66da13c11caa8b329
rgd <michael@mic-rigaud.fr>
2020-04-22 13:34:59 +0200

    [BUG] La suppression des alertes ne fonctionnait pas

--- src/plugins/nids/nids.py
+++ src/plugins/nids/nids.py
@@ -3,7 +3,7 @@
 # @Project: Major_Home
 # @Filename: nids.py
 # @Last modified by:   michael
-# @Last modified time: 16-Apr-2020
+# @Last modified time: 22-Apr-2020
 # @License: GNU GPL v3
 
 """Envoie les alarmes NIDS"""
@@ -59,8 +59,8 @@ def nids_alert():
         return ["Il y a le problème suivant:\n " + str(evenements)]
     for event in evenements:
         if event["event_type"] == "alert":
-            if event["alert"]["category"] != "NotSuspiciousTraffic":
-                message_test = str(event).replace(' ', '').replace('\n', '')
+            if event["alert"]["category"] != "Not Suspicious Traffic":
+                message_test = str(event).replace('\n', '')  # .replace(' ', '')
                 if message_test != "":
                     message.append(message_test)
     if message == []:
